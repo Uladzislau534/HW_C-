e7a2d55abc616474cd39ff39075fc7637bca0d92
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-06-24 22:08:29 +0300

    add RUS

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         int[] array = { 1, 2, 3, 4, 5 };
-
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         Console.WriteLine("Массив о обратном порядке:");
         PrintArrayInReverse(array, array.Length - 1);
     }
