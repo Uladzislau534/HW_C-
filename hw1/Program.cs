ee75c7b61c098b2695bf527a5f58f42e5ad057be
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-06-24 21:53:31 +0300

    1st commit

---
+++ hw1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int M = 5;
+        int N = 10;
+        PrintNumbers(M, N);
+    }
+
+    static void PrintNumbers(int M, int N)
+    {
+        if (M > N)
+        {
+            return;
+        }
+    
+        Console.WriteLine(M);
+     
+        PrintNumbers(M + 1, N);
+    }
+}
